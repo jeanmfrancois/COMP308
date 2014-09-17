ac9a56618c4978b5045356fd281700d8a035afdb
unknown <jeanmfrancois@hotmail.com>
2014-09-16 21:09:03 -0400

    Create Java Projects and Modify .gitignore

---
+++ program3/src/main/java/com/jfbuilds/tme1/program3/App.java
@@ -0,0 +1,13 @@
+package com.jfbuilds.tme1.program3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
