e0029dba2ea750c843f793b1fb59fdcb2f3df372
unknown <jeanmfrancois@hotmail.com>
2014-09-17 00:18:32 -0400

    Create Test Plan, Setup Program 1 for Development

---
+++ program1/src/main/java/com/jfbuilds/tme1/program1/Circle.java
@@ -0,0 +1,27 @@
+/**
+ *   File Name: Circle.java
+ *
+ *   Jean-francois Nepton
+ *   COMP 308 Java for Programmers
+ *   Cordinator: Dr. Xiaokun Zhang
+ *   Student ID# 2358976
+ *   Created: Sep 16, 2014
+ *   
+ */
+package com.jfbuilds.tme1.program1;
+
+/**
+ * Circle (description of class)
+ * <p>
+ * (description of core fields)
+ * <p>
+ * (description of core methods)
+ * 
+ * @author      Jean-francois Nepton
+ * @version     %I%, %G%
+ * @since       1.0
+ *
+ */
+public class Circle {
+
+}
